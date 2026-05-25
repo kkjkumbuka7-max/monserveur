e7f106adff7f33802c986af2a8265b95c9ce39f5
KKJ Kumbuka <kkjkumbuka7@gmail.com>
2026-05-25 22:49:43 +0200

    ajout pages html

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Serveur Flask OK 🚀"
+def login():
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
